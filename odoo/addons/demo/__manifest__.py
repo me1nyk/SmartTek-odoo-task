b4b68ad3177a53d43a160567f44fdf9f504d18fe
Serhii Melnyk <serg.melnyk1998@gmail.com>
2023-06-25 23:59:54 +0300

    - Add Demo Module with demo.demo object & "name" field - Add tree and form views to the Demo Module

---
+++ odoo/addons/demo/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Demo Module',
+    'version': '1.0',
+    'author': 'me1nyk',
+    'summary': 'Module for demo purposes',
+    'description': 'This module demonstrates the creation of a new object with name field and separate menu, tree, and form views.',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/demo.xml'
+    ]
+}
